beba14039273df3bd4a920c3b9493556dcee5bf4
franzwollang <franzwollang@gmail.com>
2020-04-18 09:07:41 +0200

    update to objects lvl 3

--- challenges/objects/level-3/objHasProp.js
+++ challenges/objects/level-3/objHasProp.js
@@ -1,3 +1,9 @@
 function objectHasProperty(object, property) {
+  if (typeof object != 'object' || object == null) {
+    throw Error('First argument must be of type Object')
+  }
+  if (typeof property != 'string') {
+    throw Error('Second argument must be of type string')
+    }
   // your code goes here
 };
